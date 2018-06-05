886b99081217106e21edadb8962cc255adee927f
Selin Onal <31602418+selingonal@users.noreply.github.com>
2018-06-05 15:24:12 -0400

    Add files via upload

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 #this is an example of a function based view
 # CRUD
@@ -11,9 +12,28 @@ from django.shortcuts import render
 def post_create(request): #Create
 	return HttpResponse("<h1> Create</h1>")
 def post_detail(request): #retrieve
-	return HttpResponse("<h1> Detail</h1>")
+	context ={
+		"title": "Detail"
+		
+	}
+	return render(request, "index.html",context)
+	#return HttpResponse("<h1> Detail</h1>")
 def post_list(request): #list items
-	return HttpResponse("<h1> List</h1>")
+	queryset= Post.objects.all()
+	context ={
+			"object_list" : queryset,
+			"title": "My User List"
+		}
+	# if request.user.is_authenticated():
+	# 	context ={
+	# 		"object_list":queryset
+	# 		"title": "My User List"
+	# 	}
+	# else: 
+	# 	context ={
+	# 		"title": "List"
+	# 	}
+	return render(request, "index.html",context)
 def post_update(request): #edit
 	return HttpResponse("<h1> Update</h1>")
 def post_delete(request): #delete
