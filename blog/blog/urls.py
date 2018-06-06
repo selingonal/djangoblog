7a2ca2315f21c8247ccdf723ab416ba6935610de
selingonal <selingonal@gmail.com>
2018-06-06 11:33:10 -0400

    deleted user and home

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,9 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/',include('users.urls')),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-    path('',include('home.urls')),
-
+    path('', admin.site.urls),
 ]
--- blog/home/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Home(models.Model):
-    icon = models.CharField(max_length= 1000)
