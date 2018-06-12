ecb451f3cdc574b6f3738824e1683dd66a28697c
selingonal <selingonal@gmail.com>
2018-06-12 13:47:34 -0400

    CSS updated, finish user permissions = done

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render
 
 # The method we are designing, this app around is CRUD
 # CREATE -- MAKE NEW
@@ -11,10 +13,10 @@ from .forms import PostForm
 # DELETE -- DELETE
 
 def post_create(request): #Create
-	# if not request.user.is_staff or not request.user.is_superuser:
-	# 	raise Http404
-	# if not request.useris_authenticated():
-	# 	raise Http404
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	if not request.useris_authenticated():
+		raise Http404
 
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
@@ -42,17 +44,26 @@ def post_detail(request, id=None): #retrieve
 
 
 def post_list(request): #list items
-	queryset= Post.objects.all()
+	queryset_list= Post.objects.all()
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	queryset = paginator.get_page(page)
+
 	context ={
 			"object_list" : queryset,
 			"title": "List"
 		}
-	return render(request, "index.html",context)
+	return render(request, "post_list.html",context)
+
+def listing(request):
+    contact_list = Contacts.objects.all()
+    return render(request, 'list.html', {'queryset': queryset_list})
 
 
 def post_update(request, id =None): #edit
-	# if not request.user.is_staff or not request.user.is_superuser:
-	# 	raise Http404
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, id=id)
 	form = PostForm(request.POST or None, request.FILES or None, instance =instance)
 	if form.is_valid():
