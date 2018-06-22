1facfbe27024b683525ea03a048103b881351da9
selingonal <selingonal@gmail.com>
2018-06-22 16:20:42 -0400

    update on comments -- doesn't run

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,5 @@
-#imports#
+#imports
+
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
@@ -8,9 +9,10 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 
-
+#please refer to post_create's comments as a reference for repetitive code
 
 def post_create(request): #Create
+	#Permissons to create a post are limited to staff, superusers, and users with author authorization
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise PermissionDenied
 
@@ -25,13 +27,23 @@ def post_create(request): #Create
 	context={
 		"form": form,
 	}
+	'''
+	returns the request the function makes to the html file "post_form.html"
+	(request // hmtl file-- located in templates // context)
+	context is a dictionary with variable names as the "key" and their values as the "value"
+	here
+	'''
 	return render(request, "post_form.html",context)
 
 
 def post_detail(request, id=None): #retrieve
+	
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise PermissionDenied
+
+	#calls the post's individual id
 	instance = get_object_or_404(Post, id=id)
+	#this displays comments under the indivual post
 	comments= Comment.objects.filter(post = id)
 
 	context ={
@@ -43,15 +55,22 @@ def post_detail(request, id=None): #retrieve
 
 
 def post_list(request): #list items
+
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise PermissionDenied
-
+	# calls to display all objects
 	queryset_list= Post.objects.all()
+	'''
+	Paginator is what separates posts into separate 
+	pages rather than having one really long list
+	'''
 	paginator = Paginator(queryset_list, 10) # Show 10 contacts per page
-	#search query
+	
+	#Search Query code
 	query= request.GET.get('q')
 	if query:
 		 queryset_list=queryset_list.filter(title__icontains=query)
+	#adds on the the paginator, renders the page we requested
 	page = request.GET.get('page')
 	queryset = paginator.get_page(page)
 
@@ -61,16 +80,20 @@ def post_list(request): #list items
 		}
 	return render(request, "post_list.html",context)
 
-def listing(request):
-    contact_list = Contacts.objects.all()
-    return render(request, 'list.html', {'queryset': queryset_list})
+# #I don't quite remember what this is or what it does 
+# def listing(request):
+#     contact_list = Contacts.objects.all()
+#     return render(request, 'post_list.html', {'queryset': queryset_list})
 
 
 def post_update(request, id =None): #edit
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise PermissionDenied
+	
 	instance = get_object_or_404(Post, id=id)
+	
 	form = PostForm(request.POST or None, request.FILES or None, instance =instance)
+	
 	if form.is_valid():
 		instance = form.save(commit = False)
 		instance.save()
@@ -78,8 +101,6 @@ def post_update(request, id =None): #edit
 		form=PostForm()
 		return HttpResponseRedirect(instance.get_absolute_url())
 	context={
-		# "title": instance.title,
-		# "instance": instance,
 		"form": form,
 	}
 	return render(request, "post_form.html", context)
@@ -117,7 +138,7 @@ def add_comment(request, pk=None):
 		'form':form
 		}
 	return render(request,'add_comment.html', context)
-
+'''
 
 def view_comment(request,pk=None):
 	if not request.user.is_staff or not request.user.is_superuser:
@@ -161,4 +182,4 @@ def comment_delete(request, pk=None):
 		"form":form,
 	}
 	return HttpResponseRedirect(post.get_absolute_url())
-
+'''
